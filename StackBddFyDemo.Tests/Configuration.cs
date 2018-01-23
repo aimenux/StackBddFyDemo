450655eadc22d665597057f0d5236a7f1f9abe4d
devpp <devpp@home.com>
2018-01-23 22:07:02 +0100

    EnableDiagnostics & ProduceDocumentation

---
+++ StackBddFyDemo.Tests/Configuration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.BDDfy.Configuration;
+
+namespace StackBddFyDemo.Tests
+{
+    [TestClass]
+    public class Configuration
+    {
+        [AssemblyInitialize]
+        public static void Initialize(TestContext context)
+        {
+            EnableDiagnostics();
+        }
+
+        private static void EnableDiagnostics()
+        {
+            Configurator.BatchProcessors.DiagnosticsReport.Enable();
+        }
+
+        private static void ProduceDocumentation()
+        {
+            Configurator.Processors.TestRunner.Disable();
+        }
+    }
+}
